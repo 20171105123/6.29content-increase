66a038d866027c2a2107a443f08e6e1f5f92c6bd
20171105123 <s20171105123@admindeimac-34.local>
2018-06-29 08:28:04 +0800

    Initial Commit

---
+++ 6.29content increase/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  6.29content increase
+//
+//  Created by 鲁祥 on 2018/6/29.
+//  Copyright © 2018年 鲁祥. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
